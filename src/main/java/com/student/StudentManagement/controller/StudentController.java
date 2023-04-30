8099cceee3dd7f7df392c5e4feed44e7e52a39c2
mohsine1999 <mohsine090hajjar@gmail.com>
2023-04-30 12:35:31 +0100

    crud operations are done !!!   there is no need to the update method now ... Stay tuned

--- src/main/java/com/student/StudentManagement/controller/StudentController.java
+++ src/main/java/com/student/StudentManagement/controller/StudentController.java
@@ -50,7 +50,7 @@ public class StudentController {
         return studentService.getStudentByApogee(apogee);
 
     }
-    //hello world ...
+    //hello world ... hh
 
 
     @GetMapping("/deleteStudent/{apogee}")
